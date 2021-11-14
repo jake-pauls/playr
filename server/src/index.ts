276de6e799c4b2332c22be2a0661fb7c95082d31
jake-pauls <jakepauls99@gmail.com>
2021-11-13 22:35:47 -0800

    Dockerized basic mongo configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
+import { config } from 'dotenv';
 import mongoose from 'mongoose';
 import app from './app';
 
 const main = async () => {
+  config();
   const port: string | number = process.env.PORT || 4040;
 
   let db;
   try {
-    db = await mongoose.connect(
-      `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}`
-    );
+    db = await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:27017`);
 
     console.log(`Connected to MongoDB on ${db}`);
   } catch (err) {
@@ -18,10 +18,6 @@ const main = async () => {
   app.listen(port, () => {
     console.log(`Express server started on https://localhost:${port}`);
   });
-
-  // Verify the contents of your .env
-  // import { config } from 'dotenv';
-  // console.log(config().parsed);
 };
 
 main().catch((err) => {
--- server/src/routes/playtest.ts
+++ server/src/routes/playtest.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Router } from 'express';
 import * as playtestController from '../controllers/playtest';
 
 const playtest: Router = express.Router();
